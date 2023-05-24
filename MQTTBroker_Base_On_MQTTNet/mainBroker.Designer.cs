2880f7a3d3e74018b8ece3d3b8d107a3b5aa7596
Jen Chih Tsai <51697896+HanksJCTsai@users.noreply.github.com>
2023-05-24 17:58:08 +0800

    push code for ui.

--- MQTTBroker_Base_On_MQTTNet/mainBroker.Designer.cs
+++ MQTTBroker_Base_On_MQTTNet/mainBroker.Designer.cs
@@ -28,37 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.sidePanel = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSetting = new FontAwesome.Sharp.IconButton();
+            this.btnMarketing = new FontAwesome.Sharp.IconButton();
+            this.btnCustomers = new FontAwesome.Sharp.IconButton();
+            this.btnProducts = new FontAwesome.Sharp.IconButton();
+            this.btnOrders = new FontAwesome.Sharp.IconButton();
+            this.btnDashboard = new FontAwesome.Sharp.IconButton();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.lblUserName = new System.Windows.Forms.Label();
             this.picBoxAvatar = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.sidePanel.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxAvatar)).BeginInit();
             this.SuspendLayout();
             // 
-            // sidePanel
+            // panelMenu
             // 
-            this.sidePanel.Controls.Add(this.button1);
-            this.sidePanel.Controls.Add(this.panel2);
-            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.sidePanel.Location = new System.Drawing.Point(0, 0);
-            this.sidePanel.Name = "sidePanel";
-            this.sidePanel.Size = new System.Drawing.Size(186, 577);
-            this.sidePanel.TabIndex = 0;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelMenu.Controls.Add(this.btnSetting);
+            this.panelMenu.Controls.Add(this.btnMarketing);
+            this.panelMenu.Controls.Add(this.btnCustomers);
+            this.panelMenu.Controls.Add(this.btnProducts);
+            this.panelMenu.Controls.Add(this.btnOrders);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 670);
+            this.panelMenu.TabIndex = 0;
             // 
-            // panel2
+            // btnSetting
             // 
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.lblUserName);
-            this.panel2.Controls.Add(this.picBoxAvatar);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(186, 150);
-            this.panel2.TabIndex = 0;
+            this.btnSetting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetting.FlatAppearance.BorderSize = 0;
+            this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetting.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnSetting.IconChar = FontAwesome.Sharp.IconChar.Tools;
+            this.btnSetting.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnSetting.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSetting.IconSize = 32;
+            this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.Location = new System.Drawing.Point(0, 450);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnSetting.Size = new System.Drawing.Size(220, 60);
+            this.btnSetting.TabIndex = 6;
+            this.btnSetting.Text = "Setting";
+            this.btnSetting.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSetting.UseVisualStyleBackColor = true;
+            // 
+            // btnMarketing
+            // 
+            this.btnMarketing.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMarketing.FlatAppearance.BorderSize = 0;
+            this.btnMarketing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMarketing.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnMarketing.IconChar = FontAwesome.Sharp.IconChar.MailBulk;
+            this.btnMarketing.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnMarketing.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMarketing.IconSize = 32;
+            this.btnMarketing.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMarketing.Location = new System.Drawing.Point(0, 390);
+            this.btnMarketing.Name = "btnMarketing";
+            this.btnMarketing.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnMarketing.Size = new System.Drawing.Size(220, 60);
+            this.btnMarketing.TabIndex = 5;
+            this.btnMarketing.Text = "Marketing";
+            this.btnMarketing.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMarketing.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMarketing.UseVisualStyleBackColor = true;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomers.FlatAppearance.BorderSize = 0;
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnCustomers.IconChar = FontAwesome.Sharp.IconChar.AddressBook;
+            this.btnCustomers.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnCustomers.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCustomers.IconSize = 32;
+            this.btnCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.Location = new System.Drawing.Point(0, 330);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnCustomers.Size = new System.Drawing.Size(220, 60);
+            this.btnCustomers.TabIndex = 4;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProducts.FlatAppearance.BorderSize = 0;
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnProducts.IconChar = FontAwesome.Sharp.IconChar.Tag;
+            this.btnProducts.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnProducts.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnProducts.IconSize = 32;
+            this.btnProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.Location = new System.Drawing.Point(0, 270);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnProducts.Size = new System.Drawing.Size(220, 60);
+            this.btnProducts.TabIndex = 3;
+            this.btnProducts.Text = "Products";
+            this.btnProducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrders.FlatAppearance.BorderSize = 0;
+            this.btnOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrders.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnOrders.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.btnOrders.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnOrders.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnOrders.IconSize = 32;
+            this.btnOrders.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrders.Location = new System.Drawing.Point(0, 210);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnOrders.Size = new System.Drawing.Size(220, 60);
+            this.btnOrders.TabIndex = 2;
+            this.btnOrders.Text = "Orders";
+            this.btnOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrders.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOrders.UseVisualStyleBackColor = true;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnDashboard.IconChar = FontAwesome.Sharp.IconChar.ChartColumn;
+            this.btnDashboard.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnDashboard.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDashboard.IconSize = 32;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 150);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnDashboard.Size = new System.Drawing.Size(220, 60);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Controls.Add(this.lblUserName);
+            this.panelLogo.Controls.Add(this.picBoxAvatar);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 150);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.label1.Location = new System.Drawing.Point(37, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Some User Text here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblUserName
             // 
@@ -82,42 +232,21 @@
             this.picBoxAvatar.TabIndex = 0;
             this.picBoxAvatar.TabStop = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
-            this.label1.Location = new System.Drawing.Point(37, 124);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Some User Text here";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(52, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // mainBroker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(951, 577);
-            this.Controls.Add(this.sidePanel);
+            this.ClientSize = new System.Drawing.Size(1200, 670);
+            this.Controls.Add(this.panelMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "mainBroker";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MQTT Broker";
-            this.sidePanel.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxAvatar)).EndInit();
             this.ResumeLayout(false);
 
@@ -125,12 +254,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel sidePanel;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.PictureBox picBoxAvatar;
         private System.Windows.Forms.Label lblUserName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private FontAwesome.Sharp.IconButton btnDashboard;
+        private FontAwesome.Sharp.IconButton btnSetting;
+        private FontAwesome.Sharp.IconButton btnMarketing;
+        private FontAwesome.Sharp.IconButton btnCustomers;
+        private FontAwesome.Sharp.IconButton btnProducts;
+        private FontAwesome.Sharp.IconButton btnOrders;
     }
 }
 
--- MQTTBroker_Base_On_MQTTNet/mainBroker.cs
+++ MQTTBroker_Base_On_MQTTNet/mainBroker.cs
@@ -2,19 +2,83 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace MQTTBroker_Base_On_MQTTNet
 {
+    //https://www.youtube.com/watch?v=5AsJJl7Bhvc
     public partial class mainBroker : Form
     {
+        //UI Fields
+        private IconButton currentButn;
+        private Panel leftBoarderBtn;
         public mainBroker()
         {
             InitializeComponent();
+            leftBoarderBtn = new Panel();
+            leftBoarderBtn.Size = new Size(7,60);
+            panelMenu.Controls.Add(leftBoarderBtn);
+            this.btnDashboard.Click += new System.EventHandler(this.ClickEventButton);
+        }
+
+        //Structs
+        private struct RGBColors
+        {
+            public static Color color172 = Color.FromArgb(172, 126, 241);
+            public static Color color249 = Color.FromArgb(249, 118, 176);
+            public static Color color253 = Color.FromArgb(253, 138, 114);
+            public static Color color95 = Color.FromArgb(95, 77, 221);
+            public static Color color24988 = Color.FromArgb(249, 88, 155);
+            public static Color color24 = Color.FromArgb(24, 161, 251);
+        }
+
+        //Methods
+        private void ActivateButton(object senderBtn, Color CurrentColor)
+        { 
+            if(senderBtn != null)
+            {
+                DisableButton();
+                //Button
+                currentButn = senderBtn as IconButton;
+                currentButn.BackColor = Color.FromArgb(37,36,81);
+                currentButn.ForeColor = CurrentColor;
+                currentButn.IconColor = CurrentColor;
+                currentButn.TextAlign = ContentAlignment.MiddleCenter;
+                currentButn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentButn.ImageAlign = ContentAlignment.MiddleRight;
+                //Left board
+                leftBoarderBtn.BackColor = CurrentColor;
+                leftBoarderBtn.Location = new Point(0, currentButn.Location.Y);
+                leftBoarderBtn.Visible = true;
+                currentButn.BringToFront();
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentButn != null)
+            {
+                currentButn.BackColor = Color.FromArgb(31, 30, 68);
+                currentButn.ForeColor = Color.Gainsboro;
+                currentButn.IconColor = Color.Gainsboro;
+                currentButn.TextAlign = ContentAlignment.MiddleLeft;
+                currentButn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentButn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void ClickEventButton(object sender, EventArgs e)
+        {
+           if (sender!= null)
+            {
+                ActivateButton(sender, RGBColors.color172);
+            }
         }
     }
 }
