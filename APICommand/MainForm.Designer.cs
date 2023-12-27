785bea35830627c1ffd56023d0e05f9d113948fe
JCTsai0 <JCTsai0@winbond.com>
2023-12-27 18:30:01 +0800

    Add API Page

--- APICommand/MainForm.Designer.cs
+++ APICommand/MainForm.Designer.cs
@@ -31,23 +31,38 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
             this.txtCommandInfo = new System.Windows.Forms.TextBox();
-            this.txtInputCommand = new System.Windows.Forms.TextBox();
+            this.splitContainerOpenAPIFile = new System.Windows.Forms.SplitContainer();
+            this.txtAPIPath = new System.Windows.Forms.TextBox();
+            this.btnSelectAPIPAth = new System.Windows.Forms.Button();
+            this.splitContainerStartAPI = new System.Windows.Forms.SplitContainer();
+            this.txtCMD = new System.Windows.Forms.TextBox();
+            this.btnSendCMD = new System.Windows.Forms.Button();
             this.tableLayoutPanelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerOpenAPIFile)).BeginInit();
+            this.splitContainerOpenAPIFile.Panel1.SuspendLayout();
+            this.splitContainerOpenAPIFile.Panel2.SuspendLayout();
+            this.splitContainerOpenAPIFile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerStartAPI)).BeginInit();
+            this.splitContainerStartAPI.Panel1.SuspendLayout();
+            this.splitContainerStartAPI.Panel2.SuspendLayout();
+            this.splitContainerStartAPI.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanelMain
             // 
             this.tableLayoutPanelMain.ColumnCount = 1;
             this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelMain.Controls.Add(this.txtCommandInfo, 0, 0);
-            this.tableLayoutPanelMain.Controls.Add(this.txtInputCommand, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.txtCommandInfo, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.splitContainerOpenAPIFile, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.splitContainerStartAPI, 0, 2);
             this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
-            this.tableLayoutPanelMain.RowCount = 2;
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.45313F));
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.54688F));
+            this.tableLayoutPanelMain.RowCount = 3;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.149011F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.85099F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 69F));
             this.tableLayoutPanelMain.Size = new System.Drawing.Size(1024, 768);
             this.tableLayoutPanelMain.TabIndex = 0;
             // 
@@ -58,27 +73,116 @@
             this.txtCommandInfo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtCommandInfo.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCommandInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(194)))), ((int)(((byte)(14)))));
-            this.txtCommandInfo.Location = new System.Drawing.Point(2, 2);
+            this.txtCommandInfo.Location = new System.Drawing.Point(2, 58);
             this.txtCommandInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCommandInfo.Multiline = true;
             this.txtCommandInfo.Name = "txtCommandInfo";
             this.txtCommandInfo.ReadOnly = true;
-            this.txtCommandInfo.Size = new System.Drawing.Size(1020, 683);
+            this.txtCommandInfo.Size = new System.Drawing.Size(1020, 638);
             this.txtCommandInfo.TabIndex = 0;
-            this.txtCommandInfo.Text = "11111111111111111111\r\n22222222222222222222222\r\n33333333333333333333333";
             // 
-            // txtInputCommand
+            // splitContainerOpenAPIFile
             // 
-            this.txtInputCommand.BackColor = System.Drawing.Color.Gainsboro;
-            this.txtInputCommand.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtInputCommand.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtInputCommand.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtInputCommand.Location = new System.Drawing.Point(2, 689);
-            this.txtInputCommand.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtInputCommand.Multiline = true;
-            this.txtInputCommand.Name = "txtInputCommand";
-            this.txtInputCommand.Size = new System.Drawing.Size(1020, 77);
-            this.txtInputCommand.TabIndex = 1;
+            this.splitContainerOpenAPIFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainerOpenAPIFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerOpenAPIFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splitContainerOpenAPIFile.Location = new System.Drawing.Point(2, 2);
+            this.splitContainerOpenAPIFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainerOpenAPIFile.Name = "splitContainerOpenAPIFile";
+            // 
+            // splitContainerOpenAPIFile.Panel1
+            // 
+            this.splitContainerOpenAPIFile.Panel1.Controls.Add(this.txtAPIPath);
+            // 
+            // splitContainerOpenAPIFile.Panel2
+            // 
+            this.splitContainerOpenAPIFile.Panel2.Controls.Add(this.btnSelectAPIPAth);
+            this.splitContainerOpenAPIFile.Size = new System.Drawing.Size(1020, 52);
+            this.splitContainerOpenAPIFile.SplitterDistance = 800;
+            this.splitContainerOpenAPIFile.SplitterWidth = 3;
+            this.splitContainerOpenAPIFile.TabIndex = 2;
+            // 
+            // txtAPIPath
+            // 
+            this.txtAPIPath.BackColor = System.Drawing.Color.Black;
+            this.txtAPIPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAPIPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAPIPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAPIPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(194)))), ((int)(((byte)(14)))));
+            this.txtAPIPath.Location = new System.Drawing.Point(0, 0);
+            this.txtAPIPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAPIPath.Multiline = true;
+            this.txtAPIPath.Name = "txtAPIPath";
+            this.txtAPIPath.ReadOnly = true;
+            this.txtAPIPath.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtAPIPath.Size = new System.Drawing.Size(798, 50);
+            this.txtAPIPath.TabIndex = 0;
+            // 
+            // btnSelectAPIPAth
+            // 
+            this.btnSelectAPIPAth.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSelectAPIPAth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSelectAPIPAth.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSelectAPIPAth.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectAPIPAth.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.btnSelectAPIPAth.Location = new System.Drawing.Point(0, 0);
+            this.btnSelectAPIPAth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSelectAPIPAth.Name = "btnSelectAPIPAth";
+            this.btnSelectAPIPAth.Size = new System.Drawing.Size(215, 50);
+            this.btnSelectAPIPAth.TabIndex = 35;
+            this.btnSelectAPIPAth.Text = "Opne API";
+            this.btnSelectAPIPAth.UseVisualStyleBackColor = true;
+            // 
+            // splitContainerStartAPI
+            // 
+            this.splitContainerStartAPI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainerStartAPI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerStartAPI.Location = new System.Drawing.Point(2, 700);
+            this.splitContainerStartAPI.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainerStartAPI.Name = "splitContainerStartAPI";
+            // 
+            // splitContainerStartAPI.Panel1
+            // 
+            this.splitContainerStartAPI.Panel1.Controls.Add(this.txtCMD);
+            // 
+            // splitContainerStartAPI.Panel2
+            // 
+            this.splitContainerStartAPI.Panel2.Controls.Add(this.btnSendCMD);
+            this.splitContainerStartAPI.Size = new System.Drawing.Size(1020, 66);
+            this.splitContainerStartAPI.SplitterDistance = 800;
+            this.splitContainerStartAPI.SplitterWidth = 3;
+            this.splitContainerStartAPI.TabIndex = 3;
+            // 
+            // txtCMD
+            // 
+            this.txtCMD.BackColor = System.Drawing.Color.White;
+            this.txtCMD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCMD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCMD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCMD.ForeColor = System.Drawing.Color.Black;
+            this.txtCMD.Location = new System.Drawing.Point(0, 0);
+            this.txtCMD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCMD.Multiline = true;
+            this.txtCMD.Name = "txtCMD";
+            this.txtCMD.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCMD.Size = new System.Drawing.Size(798, 64);
+            this.txtCMD.TabIndex = 1;
+            // 
+            // btnSendCMD
+            // 
+            this.btnSendCMD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSendCMD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSendCMD.Enabled = false;
+            this.btnSendCMD.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSendCMD.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendCMD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.btnSendCMD.Location = new System.Drawing.Point(0, 0);
+            this.btnSendCMD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSendCMD.Name = "btnSendCMD";
+            this.btnSendCMD.Size = new System.Drawing.Size(215, 64);
+            this.btnSendCMD.TabIndex = 36;
+            this.btnSendCMD.Text = "Send CMD";
+            this.btnSendCMD.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -95,8 +199,19 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Tag = "API Command";
             this.Text = "API Command";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.tableLayoutPanelMain.ResumeLayout(false);
             this.tableLayoutPanelMain.PerformLayout();
+            this.splitContainerOpenAPIFile.Panel1.ResumeLayout(false);
+            this.splitContainerOpenAPIFile.Panel1.PerformLayout();
+            this.splitContainerOpenAPIFile.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerOpenAPIFile)).EndInit();
+            this.splitContainerOpenAPIFile.ResumeLayout(false);
+            this.splitContainerStartAPI.Panel1.ResumeLayout(false);
+            this.splitContainerStartAPI.Panel1.PerformLayout();
+            this.splitContainerStartAPI.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerStartAPI)).EndInit();
+            this.splitContainerStartAPI.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -105,7 +220,12 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
         private System.Windows.Forms.TextBox txtCommandInfo;
-        private System.Windows.Forms.TextBox txtInputCommand;
+        private System.Windows.Forms.SplitContainer splitContainerOpenAPIFile;
+        private System.Windows.Forms.TextBox txtAPIPath;
+        private System.Windows.Forms.Button btnSelectAPIPAth;
+        private System.Windows.Forms.SplitContainer splitContainerStartAPI;
+        private System.Windows.Forms.Button btnSendCMD;
+        private System.Windows.Forms.TextBox txtCMD;
     }
 }
 
--- APICommand/MainForm.cs
+++ APICommand/MainForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,90 @@ namespace APICommand
 {
     public partial class MainForm : Form
     {
+        private OpenFileDialog openAPIFileDialog = new OpenFileDialog();
+        private string defaultFilePath = string.Empty;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        // write out info to the display window
+        private void build_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string strMessage = e.Data;
+            if (string.IsNullOrEmpty(strMessage))
+            {
+                this.txtCommandInfo.Text += strMessage;
+            }
+        }
+
+        private EventHandler CreateEventButton_SendCMD()
+        {
+            return (sender, e) =>
+            {
+                Process mainProcess = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = this.txtAPIPath.Text,                    // 執行檔路徑
+                        Arguments = this.txtCMD.Text.Trim(),                // 執行時傳入的引數
+                        UseShellExecute = false,                 // 是否要使用作業系統 shell 來啟動處理程序
+                        CreateNoWindow = false,                  // 是否要在新視窗中啟動處理程序
+                        WindowStyle = ProcessWindowStyle.Hidden, // 新視窗的顯示方式
+                        RedirectStandardOutput = true,           // 應用程式的文字輸出是否寫入至 StandardOutput 資料流
+                        RedirectStandardError = true,            // 應用程式的錯誤輸出是否寫入至 StandardError 資料流
+                    },
+                };
+                mainProcess.ErrorDataReceived += this.build_ErrorDataReceived;
+                mainProcess.OutputDataReceived += this.build_ErrorDataReceived;
+                mainProcess.EnableRaisingEvents = true;
+                mainProcess.Start();
+                mainProcess.BeginOutputReadLine();
+                mainProcess.BeginErrorReadLine();
+                mainProcess.WaitForExit();
+            };
+        }
+
+        private EventHandler CreateEventButton_OpenAPI()
+        {
+            return (sender, e) =>
+            {
+                openAPIFileDialog = new OpenFileDialog();
+                openAPIFileDialog.Title = "Select API file path";
+                openAPIFileDialog.InitialDirectory = defaultFilePath;
+                openAPIFileDialog.Filter = "API files (*.*)|*.exe";
+                if (openAPIFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.txtAPIPath.Text = System.IO.Path.GetFullPath(openAPIFileDialog.FileName);
+                    if (openAPIFileDialog.CheckFileExists)
+                    {
+                        this.btnSendCMD.Enabled = true;
+                    }
+                }
+            };
+        }
+
+        private void AddButtonClickEvent()
+        {
+            ButtonFactory.CreateButtonEvent(this.btnSelectAPIPAth, this.CreateEventButton_OpenAPI());
+            ButtonFactory.CreateButtonEvent(this.btnSendCMD, this.CreateEventButton_SendCMD());
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.AddButtonClickEvent();
+        }
+
+    }
+    public static class ButtonFactory
+    {
+        public static void CreateButtonEvent(Button triggerButton, EventHandler clickEvent)
+        {
+            if (triggerButton != null && clickEvent != null)
+            {
+                triggerButton.Click += clickEvent;
+            }
         }
     }
 }
